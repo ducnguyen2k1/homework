33cc267532573743fbe114dcdd154b60583eed2b
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-03-14 19:00:10 +0700

    bai tap 4

---
+++ homework4/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter your name: ");
+string yourName = Console.ReadLine();
+Console.WriteLine("Hello: " + yourName);
