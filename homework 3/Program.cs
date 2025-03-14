7fd8d0bc15ffdf30a19d55eebd3cf5cea373908e
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-03-14 18:49:58 +0700

    thuc hanh 2

---
+++ homework 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TimeLine
+{
+    class Program
+    {
+      
+        static void Main()
+        {
+              float width, height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area );
+        }
+    }
+}
