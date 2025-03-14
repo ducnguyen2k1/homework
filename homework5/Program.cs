359cccfd07a7782f6a2a99b2c2ac1309d8da88f7
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-03-14 19:04:08 +0700

    bai tap 5

---
+++ homework5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rate = 23000;
+
+            double usd;
+
+            Console.WriteLine("print USD money: ");
+            usd = Convert.ToDouble(Console.ReadLine());
+
+            double vnd = usd * rate;
+
+            Console.WriteLine($" number money {usd} USD is {vnd} VND.");
+        }
+    }    
+}
