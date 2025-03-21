33a0d3551798e66eb14e8fdd1a5dcfef81e59711
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-03-21 16:03:38 +0700

    20 so nguyen to

---
+++ 20 so nguyen to/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Nhap so luong nguyen to: ");
+        int numbers = int.Parse(Console.ReadLine());
+
+        
+        int count = 0;
+
+        
+        int s = 2;
+
+        
+        while (count < numbers)
+        {
+            
+            if (IsPrime(s))
+            {
+                
+                Console.Write(s + " ");
+                count++;
+            }
+
+            // 
+            s++;
+        }
+    }
+
+    
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false; 
+        }
+
+        return true; 
+    }
+}
