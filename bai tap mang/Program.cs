e4c823ba17eadeda82123eced1a49152862db4a5
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-03-21 15:51:12 +0700

    bai tap mang

---
+++ bai tap mang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    public int number;
+    public string nameStr = null;
+
+    public static void Main(string[] args)
+    {
+        int[] arrNames = null;
+        arrNames = new int[] { 5, 7, 9 };
+        arrNames = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        arrNames = new int[5] { 1, 2, 4, 4, 5 };
+        arrNames = new int[5];
+        string[] arrStr;
+        arrNames[1] = 3;
+        for (int i = 0; i < arrNames.Length; i++)
+        {
+            Console.WriteLine(arrNames[i]);
+        }
+
+        foreach (int value in arrNames)
+        {
+            Console.WriteLine("value");
+        }
+
+        int number1;
+        number1 = 2;
+    }
+}
+
