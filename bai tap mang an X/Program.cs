b2378bb30b2206f9da68e53678e356d19ee86501
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-03-21 15:50:21 +0700

    bai tap mang an X

---
+++ bai tap mang an X/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main() {
+        int[] arr = new int[10];
+
+        Console.WriteLine("mang ban dau Value: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("x la so can chen: ");
+        int X = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("nhap index (0 -> 9) can chen X: ");
+        int index = int.Parse(Console.ReadLine());
+
+        if (index < 1 || index >= arr.Length - 1)
+        {
+            Console.WriteLine("khong the chen phan tu.");
+            return;
+        }
+
+        for (int i = arr.Length - 1;i > index; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = X;
+
+        Console.WriteLine("dung new Value (mang sau khi chen: ");
+        for(int i = 0;i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i] + "");
+        }
+        PrintArray(arr);
+    }
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
