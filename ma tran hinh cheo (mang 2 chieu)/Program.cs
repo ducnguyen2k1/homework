fbe0a7e3aaca252dee32ce4f2d5249aab5f43aa3
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-04-04 19:31:42 +0700

    mang trong C# (da chieu)

---
+++ ma tran hinh cheo (mang 2 chieu)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Nhap kich thuoc cua ma tran (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[n, n];
+
+        
+        Console.WriteLine("Nhap phan tu ma tran:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"Nhap phan tu o vi tri [{i + 1}, {j + 1}]: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+      
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += matrix[i, i];  
+        }
+
+        Console.WriteLine($"Tong cac phan tu cheo chinh: {sum}");
+    }
+}
+
