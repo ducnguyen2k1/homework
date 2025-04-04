827b02800a9a5e7477302aa2dd04843b496ea2b2
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-04-04 19:24:57 +0700

    lop va doi tuong

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class StopWatch
+{
+    private DateTime startTime;
+    private DateTime endTime;
+
+    
+    public DateTime StartTime
+    {
+        get { return startTime; }
+    }
+
+    public DateTime EndTime
+    {
+        get { return endTime; }
+    }
+
+    
+    public StopWatch()
+    {
+        startTime = DateTime.Now;
+        endTime = DateTime.Now;
+    }
+
+   
+    public void Start()
+    {
+        startTime = DateTime.Now;
+    }
+
+   
+    public void Stop()
+    {
+        endTime = DateTime.Now;
+    }
+
+    
+    public double GetElapsedTime()
+    {
+        return (endTime - startTime).TotalMilliseconds;
+    }
+
+
+}
