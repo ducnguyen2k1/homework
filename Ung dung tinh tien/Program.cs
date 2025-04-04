b2e7c0ca4849a702788b5e80947a705ce9c9df36
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-04-04 19:22:33 +0700

    cau lenh lap (1)

---
+++ Ung dung tinh tien/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double money = 1.0;
+        int month = 1;
+        double intersetRate = 1.0;
+
+        Console.WriteLine("Enter investment amount: ");
+        money = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter number of months: ");
+        month = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter annual interest rate in percentage: ");
+        intersetRate = double.Parse(Console.ReadLine());
+
+        double totalInterset = 0;
+        for (int i = 0; i < month; i++)
+        {
+            totalInterset += money * (intersetRate / 100) / 12;
+        }
+
+        Console.WriteLine("Total of interset: " + totalInterset);
+
+        money = 100000;
+        intersetRate = 5.5;
+        month = 3;
+
+        totalInterset = 0;
+        for (int i = 0;i < month; i++)
+        {
+            totalInterset += money * (intersetRate / 100) / 12;
+        }
+
+        Console.WriteLine("Total interest for 3 months: " + totalInterset);
+    }
+}
