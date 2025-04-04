c95d3e6e6e90fe7cbba5a8cd2dc40d835529315a
Nguyen Minh Duc <ducnguyensvip@gmail.com>
2025-04-04 19:26:19 +0700

    Ham va phuong thuc trong C#

---
+++ phuong thuc dem so/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+
+    static int CountCharacterOccurrences(string str, char ch)
+    {
+        int count = 0; 
+        
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ch) 
+            {
+                count++; 
+            }
+        }
+        return count; 
+    }
+
+    static void Main(string[] args)
+    {
+        
+        string str = "Hello, how are you today?";
+
+        
+        Console.Write("ky tu can dem: ");
+        char characterToCount = Console.ReadKey().KeyChar; 
+        Console.WriteLine(); 
+
+        
+        int count = CountCharacterOccurrences(str, characterToCount);
+
+        
+        Console.WriteLine($"Ky tu '{characterToCount}' xuat hien {count} lan trong chuoi.");
+    }
+}
+
